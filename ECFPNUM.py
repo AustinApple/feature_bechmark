70b21c2d8b7e91e222d0a057fa65fd1a557bd6c2
AustinApple <news012147stw@gmail.com>
2020-04-15 16:18:26 +0800

    finish ECFPNUM test script

---
+++ ECFPNUM.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+from rdkit.Chem import AllChem
+from rdkit import Chem, DataStructs
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn import utils
+from sklearn.metrics import mean_squared_error, mean_absolute_error 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import backend as K
+from add_function_group.feature import molecules
+# K.tensorflow_backend._get_available_gpus()
+import argparse
+
+
+
+def train(input_file, epochs, random_seed):
+    data = pd.read_csv(input_file)
+    
+    np.random.seed(random_seed)
+    random_index = np.random.permutation(len(data))
+    data = data.iloc[random_index]
+    train, validation, test = np.split(data, [int(0.8*len(data)), int(0.9*len(data))])
+    
+    x_train = molecules(train['smiles'].tolist()).ECFP_num()
+    y_train = train['IE'].values
+    
+    x_val = molecules(validation['smiles'].tolist()).ECFP_num()
+    y_val = validation['IE'].values
+
+    x_test = molecules(test['smiles'].tolist()).ECFP_num()
+    
+    
+    def rmse(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+    # building model
+    model = Sequential()
+    model.add(Dense(output_dim=int(X_train.shape[1]/2), input_dim=X_train.shape[1],activation='relu'))
+    model.add(Dense(output_dim=int(X_train.shape[1]/6),activation='relu'))
+    model.add(Dense(output_dim=int(X_train.shape[1]/12),activation='relu'))
+    model.add(Dense(output_dim=y_train.shape[1]))
+    model.compile(loss='mae', optimizer='adam',metrics=['mae',rmse],)
+    print('Training -----------')
+    model.fit(x_train, y_train, verbose=1, epochs=epochs, validation_data=[x_val, y_val])
+    
+    print("test testing set")
+    return model.evaluate(x_test, y_test)
+    
+
+# prediction = mlp.predict(X_test)
+# print(mean_absolute_error(y_test, prediction))
+# print(mean_squared_error(y_test, prediction))
+
+    # save model
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--data_file',
+                        help='the file including smiles and corresponding IE and EA', default='MP_clean_canonize_cut.csv')
+    parser.add_argument('-e', '--epochs',
+                        help='how many epochs would be trained', default=100, type=int)
+    args = vars(parser.parse_args())
+    
+    log = np.zeros((10,2))
+
+    for i in range(10):
+        mae, rmse = train(random_seed=i)
+        log[i,0] = mae
+        log[i,1] = rmse
+
+    print('mean : '+str(np.mean(log, axis=0)))
+    print('std : '+str(np.std(log, axis=0)))
+
+
